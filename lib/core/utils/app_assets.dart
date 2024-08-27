2cbad00e1dbeb666b71ce6e145fc320e735779e0
Mohamed Soliman <ms3196@fayoum.edu.eg>
2024-08-27 04:08:26 +0300

    add app string and app assets

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,11 @@
+abstract class AppAssets {
+  static const String backArrow = 'assets/svgs/back_arrow.svg';
+
+  static const String basketImage = 'assets/images/basket_image.png';
+
+  static const String paypal = 'assets/svgs/paypal.svg';
+
+  static const String pay = 'assets/svgs/pay.svg';
+
+  static const String card = 'assets/svgs/card.svg';
+}
---
+++ lib/core/utils/app_string.dart
@@ -0,0 +1,12 @@
+abstract class AppString {
+  AppString._();
+
+  static const String appName = 'Checkout App',
+      myCart = 'My Cart',
+      checkout = 'Checkout',
+      orderSubtotal = 'Order Subtotal',
+      discount = 'Discount',
+      shipping = 'Shipping',
+      total = 'Total',
+      completePayment = 'Complete Payment';
+}
