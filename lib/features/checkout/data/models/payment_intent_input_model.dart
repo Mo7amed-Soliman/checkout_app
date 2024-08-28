11cd6d6a07fb5315c26fa762f4b379a4d91db372
Mohamed Soliman <ms3196@fayoum.edu.eg>
2024-08-28 04:56:37 +0300

    create payment intent model

---
+++ lib/features/checkout/data/models/payment_intent_input_model.dart
@@ -0,0 +1,15 @@
+class PaymentIntentInputModel {
+  final String amount;
+  final String currency;
+  PaymentIntentInputModel({
+    required this.amount,
+    required this.currency,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'amount': amount,
+      'currency': currency,
+    };
+  }
+}
